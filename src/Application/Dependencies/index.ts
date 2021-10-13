4883495cf42b420475659715bcc6fd4c5318a097
fedegara <fedegara91@gmail.com>
2021-10-13 10:25:01 -0300

    wip

--- src/Application/Dependencies/index.ts
+++ src/Application/Dependencies/index.ts
@@ -69,7 +69,7 @@ const DependenciesManager = (containerBuilder: ContainerBuilder) => {
             }
           : {};
         const dsn: string = settings.MONGO_AMAZON
-          ? settings.MONGODB_DSN + "/" + settings.MONGODB_DATABASE + "?tls=true&retryWrites=false"
+          ? settings.MONGODB_DSN + "/" + settings.MONGODB_DATABASE + "/?authSource=" + settings.MONGODB_DATABASE + "&tls=true&retryWrites=false"
           : settings.MONGODB_DSN;
 
         return MongoDBClientDBAL.getInstance({
