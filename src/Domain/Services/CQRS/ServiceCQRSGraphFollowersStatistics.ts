a006203813b61952a678655871d44c4cfd69a69f
Emiliano <emimarz@gmail.com>
2022-06-10 11:28:46 -0300

    BUN-13765 (#8)
    [BUGS]
    - https://bunker.atlassian.net/browse/BUN-13765
    
    [DEPENDENCIES]
    - https://github.com/BunkerDB/linkedin-ms/pull/9
    - https://github.com/BunkerDB/linkedin-tl/pull/8
    
    Co-authored-by: fedegara <fedegara91@gmail.com>
    Co-authored-by: Emiliano Martinez <emimarz@gmail.com>

--- src/Domain/Services/CQRS/ServiceCQRSGraphFollowersStatistics.ts
+++ src/Domain/Services/CQRS/ServiceCQRSGraphFollowersStatistics.ts
@@ -4,16 +4,24 @@ import { IDataGraphsDataDAO } from "../../Interfaces/IDataGraphsDataDAO";
 import { DataGraphsDataCreateInputDTO } from "../../DTO/DataGraphsDataCreateInputDTO";
 import { ServiceCQRSGraphFollowersStatisticsTransformMapper } from "../../Mappers/ServiceCQRSGraphFollowersStatisticsTransformMapper";
 import { FollowersRawDataAllInElementsDTO } from "../../DTO/FollowersRawDataAllInElementsDTO";
+import moment from "moment";
+import { DataGraphsDataDTO } from "../../DTO/DataGraphsDataDTO";
+import { LoggerInterface } from "../../../Infrastructure/Interface/LoggerInterface";
 
 export class ServiceCQRSGraphFollowersStatistics {
   private readonly _adapter: IDataGraphsDataDAO;
+  private readonly _logger: LoggerInterface;
 
-  constructor(args: { adapter: IDataGraphsDataDAO }) {
+  constructor(args: { adapter: IDataGraphsDataDAO; logger: LoggerInterface }) {
     this._adapter = args.adapter;
+    this._logger = args.logger;
   }
 
   get adapter(): IDataGraphsDataDAO {
     return this._adapter;
+  }
+  get logger(): LoggerInterface {
+    return this._logger;
   }
 
   execute(args: { rawRow: ReportRawDataAllInDTO }): PromiseB<boolean> {
@@ -29,6 +37,46 @@ export class ServiceCQRSGraphFollowersStatistics {
   }): PromiseB<DataGraphsDataCreateInputDTO> {
     const rawRow: FollowersRawDataAllInElementsDTO = args.rawRow
       .data as unknown as FollowersRawDataAllInElementsDTO;
+
+    if (rawRow.total !== undefined) {
+      const currentDate: Date = new Date(
+        moment().utc(false).subtract(1, "day").format("YYYY-MM-DD 00:00:00")
+      );
+      currentDate.setUTCHours(0, 0, 0, 0);
+      this.adapter
+        .find({
+          instance: args.rawRow.instance,
+          externalAccountId: args.rawRow.organization,
+          date: currentDate,
+        })
+        .then((todayData: DataGraphsDataDTO) => {
+          let process = false;
+          if (todayData.metrics.followers === undefined) {
+            todayData.metrics.followers = {
+              organic_followers: 0,
+              paid_followers: 0,
+              total_followers: 0,
+              lifetime_followers: rawRow.total,
+            };
+            process = true;
+          } else if (
+            todayData.metrics.followers !== undefined &&
+            todayData.metrics.followers.lifetime_followers !== rawRow.total
+          ) {
+            todayData.metrics.followers.lifetime_followers = rawRow.total;
+            process = true;
+          }
+          if (process) {
+            this.adapter
+              .upsert({
+                input: todayData as DataGraphsDataCreateInputDTO,
+              })
+              .catch((err) => {
+                this.logger.error({ message: err.message });
+              });
+          }
+        });
+    }
 
     return PromiseB.try(() => {
       return new ServiceCQRSGraphFollowersStatisticsTransformMapper().execute({
